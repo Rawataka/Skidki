7670738208358b261fe459f6c206da2c56bc1e39
X_Admin <1kostr1@mail.ru>
2025-10-21 00:39:47 +0700

    start

--- app/src/main/java/com/example/skidki/MainActivity.kt
+++ app/src/main/java/com/example/skidki/MainActivity.kt
@@ -6,25 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.skidki.ui.theme.SkidkiTheme
 
@@ -54,48 +44,15 @@ fun DemoTextPreview() {
 
 @Composable
 fun DemoScreen(modifier: Modifier = Modifier) {
-    var order by remember { mutableStateOf("") }
-    var dishCount by remember { mutableStateOf("") }
-
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxSize()
     ) {
-        // Сумма заказа
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.align(Alignment.Start)
-        ) {
-            Text(
-                text = "Сумма заказа:",
-                fontSize = 22.sp,
-                modifier = Modifier.width(150.dp)
-            )
-            TextField(
-                value = order,
-                onValueChange = { order = it },
-                modifier = Modifier.width(190.dp)
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Количество блюд
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.align(Alignment.Start)
-        ) {
-            Text(
-                text = "Количество блюд:",
-                fontSize = 22.sp,
-                modifier = Modifier.width(150.dp)
-            )
-            TextField(
-                value = dishCount,
-                onValueChange = { dishCount = it },
-                modifier = Modifier.width(150.dp)
-            )
-        }
+        Text(
+            text = "Калькулятор скидок и чаевых",
+            fontSize = 24.sp,
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
